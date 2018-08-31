2904af667cb0ef07cbce03fad42ff1eb3c081b27
tanhlefthander <tanh.lefthander@gmail.com>
2018-08-31 21:53:59 +0700

    Update 31/8/2018

---
+++ UVa/10819_TroubleOf13-Dots.cpp
@@ -0,0 +1,31 @@
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int m, n;
+int p[110], f[110];
+int dp[110][15000];
+int recurse(int i, int w) {
+    if(w > m && m < 1800)
+        return -1000;
+    if(w > m + 200)
+        return -1000;
+    if(i == n) {
+        if(w > m && w <= 2000)
+            return -1000;
+        return 0;
+    }
+    if(dp[i][w] != -1)
+        return dp[i][w];
+ 
+    return dp[i][w] = max(recurse(i + 1, w),f[i] + recurse(i + 1, w + p[i]));
+}
+ 
+int main() {
+    while(scanf("%d %d", &m, &n) == 2) {
+        for(int i = 0; i < n; i++)
+            scanf("%d %d", &p[i], &f[i]);
+        memset(dp, -1, sizeof dp);
+        printf("%d\n", recurse(0, 0));
+    }
+}
---
+++ UVa/357_LetMeCountTheWays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int US[]={0,1,5,10,25,50};
+int n;
+ll F[30005][6];
+int main(){
+	/*freopen("data.txt","r",stdin);
+	freopen("dataOut.txt","w",stdout);*/
+	for(int i=1;i<=30000;i++) F[i][1] = 1;
+	for(int i=0;i<=5;i++) F[0][i] = 1;
+	for(int i=1;i<=30000;i++){
+		for(int j=2;j<=5;j++){
+			F[i][j] = F[i][j-1];
+			if(i >= US[j])
+				F[i][j]+= F[i-US[j]][j];
+		}
+	}
+	while(cin>>n){
+		ll r = F[n][5];
+		if(r == 1) printf("There is only 1 way to produce %d cents change.\n",n);
+		else cout<<"There are "<<r<<" ways to produce "<<n<<" cents change.\n";
+	}
+	return 0;
+}
